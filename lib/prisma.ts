16675cad557cb43f6c4a2ecc95cecbfd1e86968d
jl2302 <121573213+jl2302@users.noreply.github.com>
2025-08-10 19:16:52 -0400

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ['warn', 'error'] });
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+const g = globalThis as unknown as { prisma?: PrismaClient };
+export const prisma = g.prisma ?? new PrismaClient({ log: ['warn', 'error'] });
+if (process.env.NODE_ENV !== 'production') g.prisma = prisma;
